9337fc15b0ed02e44df046adfa83354601246ac0
carloscoronad0 <carloscoronado098@gmail.com>
2021-03-28 22:31:29 -0400

    Probando nuevo algoritmo

--- converter_Server/serverConverter.c
+++ converter_Server/serverConverter.c
@@ -10,8 +10,85 @@
 
 #define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(-1); }
 
-#define BUFFER_SIZE 16500
+#define BUFFER_SIZE 1028
 #define PORT_INDEX 1
+
+void convertir(int DS, int DPos, int x, int y, int LHeader, unsigned char *BUFFER, unsigned char BUFFERaux)
+{
+	int xr;
+	// char aux;
+
+	/ Despues de haber recibido los datos, es decir, tener lleno el buffer de chars
+    // (Header protoclo proxy-converter + BMP )con la información que llega del proxy
+    // sacamos los siguientes datos:
+    /**
+    DS del bitmap con header BMP en el byte[ LHeader + 2 ] de 4 bytes de longitud.
+    DPos del inicio del bitmap está en el byte[ LHeader + 10 ] de 4 bytes de longitud.
+    x, width de la imagen, está en el byte [ LHeader + 18 ] de 4 bytes de longitud. (esta codificado como entero con signo)
+    y, height de la imagen, está en el byte [ LHeader + 22 ] de 4 bytes de longitud (esta codificado como entero con signo)
+     **/
+     //Ahora, el procedimiento que seguiremos, tiene tres pasos:
+     // Primero invertimos los bits ( 1 a 0 y 0 a 1 ) de los datos del bitmap, que es lo mismo que intercambiar los colores blanco por negro y viceversa.
+     // Segundo, invertimos las filas, intercambiando la primera con la última, según el ancho, considerando que el padding del bmp rellena el ancho a un múltiplo de 4 bytes.
+     // Tercero, rellenamos de '0's (ya sea a lo vertical, horizontal u ambos) hasta que se obtenga un bitmap que cumpla con la resolucion de 128x64.
+     //Primer paso:
+     for ( int i = DPos + LHeader; i < DS + LHeader; i ++ )
+     {
+         BUFFER [ i ] = ~( BUFFER[ i ] ); // "~" es la operacion NOR - cambiamos los bits de cada byte en los datos
+     }
+     //Segundo paso:
+     //2.1. Encontrar los bytes que usan para representar una sola fila (incluyendo padding)
+     xr = x + ( ( 32 - ( x % 32 ) ) % 32 );
+     xr /= 8;
+
+     unsigned char aux_xr[xr];
+
+     //2.2. Voltear el buffer en grupos de "xr" manteniendo su orden interno.
+     for( int i = DPos + LHeader, k = 0; k < (y / 2); i += xr, k ++ )
+     {
+         //copiamos la ultima fila de "xr" bytes correspondientes en los "xr" iniciales, mientras se hace el viceverso al mismo tiempo
+     	memcpy(aux_xr, BUFFER + i, xr);
+     	memcpy(BUFFER + i, BUFFER + DS + LHeader - ( xr * ( k + 1 ) ), xr);
+     	memcpy(BUFFER + DS + LHeader - ( xr * ( k + 1 ) ), aux_xr, xr);
+        /* for( int j = 0; j < xr; j ++ )
+         {
+             aux = BUFFER[ i + j ]; //auxiliar para guardar el dato del "inicio" para ponerlo al "final" segun el ciclo
+             BUFFER[ i + j ] = BUFFER[ DS + LHeader - ( xr * ( k + 1 ) ) + j ]; //Copiamos los "ultimos" xr bytes en los "primeros"
+             BUFFER[ DS + LHeader - ( xr * ( k + 1 ) ) + j ] = aux; //Los "primeros" a los "ultimos"
+         }
+         */
+     }
+     //Tercer paso:
+     //Recordemos que 128x64 es 128 para width y 64 para height
+     //Verificar si width cumple con la resolucion necesaria, sino llenarla de 0s, ademas de agregarla al buffer para enviar
+     //Se hace lo mismo para el height.
+
+    for( int i = 0; i < 64; i ++ )
+    {
+        if( i < y )
+        {
+        	for( int j = 0; j < 16; j ++ )
+        	{
+            	if( j < xr )
+            	{
+                	BUFFERaux[ i*16 + j ] = BUFFER[ i*xr + j ];
+            	}
+            	else
+            	{
+                	BUFFERaux[ i*16 + j ] = '0';
+            	}
+        	}
+        }
+        else
+        {
+        	for( int j = 0; j < 16; j ++ )
+        	{
+            	BUFFERaux[ i*16 + j ] = '0';
+        	}
+        }
+
+    }
+}
 
 int main(int argc, char const *argv[])
 {
@@ -24,10 +101,11 @@ int main(int argc, char const *argv[])
 	
 	int server_fd, client_fd, err;
 	struct sockaddr_in server, client;
-	char buffer[BUFFER_SIZE];
+	unsigned char buffer[BUFFER_SIZE];
 
 	// Auxi var 
-	char bufferSend[BUFFER_SIZE];
+	unsigned char bufferaux[BUFFER_SIZE];
+	unsigned char bufferSend[BUFFER_SIZE];
 	int len;
 	int widgt;
 	int height; 
@@ -37,8 +115,8 @@ int main(int argc, char const *argv[])
 	int initData;
 
 	short lenSend;
-	char heightSend;
-	char widgtSend;
+	unsigned char heightSend;
+	unsigned char widgtSend;
 
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -136,11 +214,15 @@ int main(int argc, char const *argv[])
 			memcpy(&paletaN,buffer + 56, 4); // paleta inicia en 54
 			memcpy(&paletaB,buffer + 60, 4); // paleta inicia en 54
 
+			convertir(len, initData, widgt, height, 2, buffer, bufferaux);
+
 			//cast los valores a short (2 byts)
 
-			lenSend += 2;
-			heightSend = height;
-			widgtSend = widgt;
+			lenSend = 1028;
+			heightSend = 128;
+			widgtSend = 64;
+
+			/*
 
 			//creamos el bufferSend header
 			memcpy(bufferSend, &lenSend, 2);		//2
@@ -162,6 +244,20 @@ int main(int argc, char const *argv[])
 				//printf("%c",buffer[len - i]);
 
 			}
+
+			*/
+			int offset = 0;
+
+			memcpy(bufferSend + offset, &lenSend, sizeof(lenSend));
+			offset += sizeof(lenSend);
+
+			memcpy(bufferSend + offset, &widgtSend, sizeof(widgtSend));
+			offset += sizeof(widgtSend);
+
+			memcpy(bufferSend, &heightSend, sizeof(heightSend));
+			offset += sizeof(heightSend);
+
+			memcpy(bufferSend + offset, bufferaux, 1024);
 			
 			printf("Se termino de convertir el bitmap\n");
 
